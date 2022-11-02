561b63424db22bb2d2e6d57fe1fb0bfa2ed4541d
salavat <asfanov@gmail.com>
2022-11-02 20:42:01 +0400

    fix: html files

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/scripts.js":
@@ -16,7 +15,18 @@
   \************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var normalize_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! normalize.css */ \"./node_modules/normalize.css/normalize.css\");\n/* harmony import */ var _style_minimal_css_reset_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./style/minimal-css-reset.scss */ \"./src/style/minimal-css-reset.scss\");\n/* harmony import */ var _style_base_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./style/base.scss */ \"./src/style/base.scss\");\n/* harmony import */ var _style_header_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./style/header.scss */ \"./src/style/header.scss\");\n\n\n\n\n\n\n//# sourceURL=webpack://asfanov_cv/./src/scripts.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var normalize_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! normalize.css */ \"./node_modules/normalize.css/normalize.css\");\n/* harmony import */ var _style_minimal_css_reset_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./style/minimal-css-reset.scss */ \"./src/style/minimal-css-reset.scss\");\n/* harmony import */ var _style_base_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./style/base.scss */ \"./src/style/base.scss\");\n/* harmony import */ var _style_language_switch_botton_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./style/language-switch-botton.scss */ \"./src/style/language-switch-botton.scss\");\n/* harmony import */ var _style_header_scss__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./style/header.scss */ \"./src/style/header.scss\");\n/* harmony import */ var _style_section_1_scss__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./style/section-1.scss */ \"./src/style/section-1.scss\");\n/* harmony import */ var _style_section_2_scss__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./style/section-2.scss */ \"./src/style/section-2.scss\");\n/* harmony import */ var _scripts_switch_language_js__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./scripts/switch-language.js */ \"./src/scripts/switch-language.js\");\n/* harmony import */ var _scripts_switch_language_js__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(_scripts_switch_language_js__WEBPACK_IMPORTED_MODULE_7__);\n\n\n\n\n\n\n\n\n\n\n// scripts\n\n\n\n//# sourceURL=webpack://asfanov_cv/./src/scripts.js?");
+
+/***/ }),
+
+/***/ "./src/scripts/switch-language.js":
+/*!****************************************!*\
+  !*** ./src/scripts/switch-language.js ***!
+  \****************************************/
+/***/ (() => {
+
+eval("const russians = document.querySelectorAll('.russian');\nconst englishs = document.querySelectorAll('.english');\nconst language = document.querySelectorAll('input[type=\"radio\"]');\n\n\nlanguage.forEach(input => {\n  input.addEventListener('change', switchLanguage)\n})\n\nfunction switchLanguage(element) {\n  let lang = element.target.value;\n\n  if (lang === 'ru') {\n    russians.forEach(item => {\n      item.classList.remove('hidden')\n    })\n    englishs.forEach(item => {\n      item.classList.add('hidden')\n    })\n  } else {\n    russians.forEach(item => {\n      item.classList.add('hidden')\n    })\n    englishs.forEach(item => {\n      item.classList.remove('hidden')\n    })\n  }\n  \n}\n\n\n//# sourceURL=webpack://asfanov_cv/./src/scripts/switch-language.js?");
 
 /***/ }),
 
@@ -26,6 +36,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var norm
   \**************************************************/
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
+"use strict";
 eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.css\";\n\n//# sourceURL=webpack://asfanov_cv/./node_modules/normalize.css/normalize.css?");
 
 /***/ }),
@@ -36,6 +47,7 @@ eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.css\";\n\n
   \*****************************/
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
+"use strict";
 eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.scss\";\n\n//# sourceURL=webpack://asfanov_cv/./src/style/base.scss?");
 
 /***/ }),
@@ -46,7 +58,19 @@ eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.scss\";\n\
   \*******************************/
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
+"use strict";
 eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.scss\";\n\n//# sourceURL=webpack://asfanov_cv/./src/style/header.scss?");
+
+/***/ }),
+
+/***/ "./src/style/language-switch-botton.scss":
+/*!***********************************************!*\
+  !*** ./src/style/language-switch-botton.scss ***!
+  \***********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.scss\";\n\n//# sourceURL=webpack://asfanov_cv/./src/style/language-switch-botton.scss?");
 
 /***/ }),
 
@@ -56,7 +80,30 @@ eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.scss\";\n\
   \******************************************/
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
+"use strict";
 eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.scss\";\n\n//# sourceURL=webpack://asfanov_cv/./src/style/minimal-css-reset.scss?");
+
+/***/ }),
+
+/***/ "./src/style/section-1.scss":
+/*!**********************************!*\
+  !*** ./src/style/section-1.scss ***!
+  \**********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.scss\";\n\n//# sourceURL=webpack://asfanov_cv/./src/style/section-1.scss?");
+
+/***/ }),
+
+/***/ "./src/style/section-2.scss":
+/*!**********************************!*\
+  !*** ./src/style/section-2.scss ***!
+  \**********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.scss\";\n\n//# sourceURL=webpack://asfanov_cv/./src/style/section-2.scss?");
 
 /***/ })
 
@@ -87,6 +134,30 @@ eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.scss\";\n\
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/global */
 /******/ 	(() => {
 /******/ 		__webpack_require__.g = (function() {
@@ -97,6 +168,11 @@ eval("module.exports = __webpack_require__.p + \"b88d04fba731603756b1.scss\";\n\
 /******/ 				if (typeof window === 'object') return window;
 /******/ 			}
 /******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
